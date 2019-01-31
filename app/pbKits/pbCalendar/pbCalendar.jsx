22e7c817325322fa64598b80a231679f61c8c5dd
Markie Arnold <markiearnold@gmail.com>
2019-01-31 12:26:58 -0500

    Update pbCard markdown file and include

--- app/pbKits/pbCalendar/pbCalendar.jsx
+++ app/pbKits/pbCalendar/pbCalendar.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 class pbCalendar extends React.Component {
   render() {
-    return <div className="pbCalendar">
+    return (<div className="pbCalendar">
              Hello, {this.props.name}!
-           </div>
+           </div>)
   }
 }
 
--- app/views/nitro_sg/greetings/hello.html.md
+++ app/views/nitro_sg/greetings/hello.html.md
@@ -8,3 +8,4 @@
 
 
 <%= render 'pbCalendar/pbCalendarStory' %>
+<%= render 'pbCard/pbCardStory' %>
