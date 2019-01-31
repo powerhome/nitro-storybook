57145f0bb14f15f8aaca595048729e6fdeade363
Jasper Furniss <jasper.furniss@powerhrg.com>
2019-01-31 12:24:29 -0500

    Jaspers Latest 3 Components

---
+++ app/pbKits/pbDiv/pbDiv.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+class pbCalendar extends React.Component {
+  render() {
+    return <div>YYYYYYY Hello, I am {this.props.name}!</div>;
+  }
+}
+
+export default pbCalendar;
---
+++ app/pbKits/pbSpan/index.js
@@ -0,0 +1,3 @@
+import "../pbCalendar/pbCalendar.scss";
+
+export const pbCalendar = (a, b) => a + b;
