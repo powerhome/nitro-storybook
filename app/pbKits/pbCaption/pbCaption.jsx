eca8b3fa34470116f76a8275ec551c7dc1952497
Jason Cypret <jason@jasoncypret.com>
2019-02-01 11:32:16 -0500

    Captions are in

---
+++ app/pbKits/pbCaption/pbCaption.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+class pbCaption extends React.Component {
+  render() {
+    return (
+      <p className="caption">{this.props.text}</p>
+    )
+  }
+}
+
+export default pbCaption;
