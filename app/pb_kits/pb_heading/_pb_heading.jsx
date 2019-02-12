4afc3362a66a5a25346cc74af673d31b073a4f4e
Jasper Furniss <jasper.furniss@powerhrg.com>
2019-02-12 15:52:22 -0500

    Latest Heading Component

--- app/pb_kits/pb_heading/_pb_heading.jsx
+++ app/pb_kits/pb_heading/_pb_heading.jsx
@@ -5,21 +5,21 @@ import PropTypes from 'prop-types'
 const propTypes = {
   text: PropTypes.string.isRequired,
   tag: PropTypes.string.isRequired,
-  size: PropTypes.number.isRequired,
-  className: PropTypes.string.isRequired
+  size: PropTypes.oneOf([1, 2, 3, 4]),
+  className: PropTypes.string,
+  theme: PropTypes.string
 };
 
 const defaultProps = {
   text: 'Hello World',
-  tag: 'h1',
-  size: 1
+  size: 1,
+  theme: "dark"
 };
-
 
 class Heading extends Component {
   render() {
     return (
-      <div className={this.props.className}>{this.props.text}</div>
+      <div className={`heading-${size}-${theme}`}>{this.props.text}</div>
     )
   }
 }
