ea5cf268d12e147a93ce334e59ee0181b83da9e2
Jasper Furniss <jasper.furniss@powerhrg.com>
2019-02-18 11:58:27 -0500

    Finished Caption React Component and Added More for Vertical Nav React Component

--- app/pb_kits/nitro_sg/pb_vertical_nav_list/Item.jsx
+++ app/pb_kits/nitro_sg/pb_vertical_nav_list/Item.jsx
@@ -1,21 +1,28 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 
-
 const propTypes = {
   text: PropTypes.string.isRequired,
+  link: PropTypes.string,
+  active: PropTypes.bool
 };
 
 const defaultProps = {
   text: "Hello Item",
+  link: "#",
+  active: false
 };
 
 class Item extends Component {
   render() {
-    const { text } = this.props;
+    const { text, link, active } = this.props;
     return (
-      <li>
-        {text}
+      <li className={"vertical-nav-list-border-item" + active}>
+        <a className="vertical-nav-list-item-link"
+            href={link}
+        >
+          <span className="vertical-nav-list-item-link-text">{text}</span>
+        </a>
       </li>
     );
   }
--- app/pb_kits/nitro_sg/pb_vertical_nav_list/List.jsx
+++ app/pb_kits/nitro_sg/pb_vertical_nav_list/List.jsx
@@ -1,21 +1,24 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import Item from "./Item";
-
-
+import Caption from "../pb_caption/_caption.jsx";
 
 const propTypes = {
   children: PropTypes.element.isRequired
 };
 
-
 class List extends Component {
   static Item = Item;
   render() {
     return (
-      <ul >
-        {this.props.children}
-      </ul>
+      <div className="vertical-nav-list">
+        <div className="vertical-nav-list-title">
+          <a>
+            <Caption>{"Refactor this to show caption component later"}</Caption>
+          </a>
+        </div>
+        <ul>{this.props.children}</ul>
+      </div>
     );
   }
 }
