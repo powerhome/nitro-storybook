bcd0b75ea56496e6fdab72768056ecddfc6fcfb3
Jasper Furniss <jasper.furniss@powerhrg.com>
2019-02-22 09:31:31 -0500

    Added Support for Decorators to Babel Config

--- app/pb_kits/nitro_sg/packs/pb_image.js
+++ app/pb_kits/nitro_sg/packs/pb_image.js
@@ -1,8 +1,5 @@
 import Image from "pb_image/_image.jsx";
 import WebpackerReact from "webpacker-react";
 
-const images = require.context('', true)
-const imagePath = (name) => images(name, true)
-
 
 WebpackerReact.setup({ Image });
--- app/pb_kits/nitro_sg/pb_image/_image.jsx
+++ app/pb_kits/nitro_sg/pb_image/_image.jsx
@@ -1,64 +1,26 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import ProgressiveImage from 'react-progressive-image';
-import image from 'images/giant.jpg';
-import cool from  'images/clark.jpg';
-// import { imagePath } from '../packs/pb_image.js'
 
-
-const img = new Image(500, 300);
-const img.onload = function() {
-  // Here, you can reliably use `width` and `height`
-  console.log("Loaded, size is " + this.width + "x" + this.height);
-};
-const img.src = 'images/giant.jpg';
-
-
-// Custom Features of React-Progressive-Image
-const compressedImage = cool.preSrc;
-const palette = cool.palette;
-const fullImage = cool.src;
-const dominantImageColor = cool.palette[0];
-
-const placeholder = (
-  <div
-      style={{ backgroundColor: dominantImageColor, height: fullImage.height, width: fullImage.width }}
-  />
-);
-
-// Show me what I got!
-console.log("fullImage:" + fullImage);
-console.log("image:" + cool);
-console.log("compressedImage:" + compressedImage);
-console.log(placeholder);
 
 const propTypes = {
-  url: PropTypes.string.isRequired,
-  compressedImage: PropTypes.string,
-  palette: PropTypes.arrayOf(PropTypes.string),
-  fullImage: PropTypes.string,
-  dominantImageColor: PropTypes.string,
+  url: PropTypes.string.isRequired
 };
 
 const defaultProps = {
-  url: image,
-  compressedImage: compressedImage,
-  palette: palette,
-  fullImage: fullImage,
-  dominantImageColor: ''
+  url: ''
 };
 
+@lazyload({
+  height: 200,
+  once: true,
+  offset: 100
+})
 class Image extends Component {
   render() {
-    const { fullImage, compressedImage, palette } = this.props;
+    const { url } = this.props;
     return (  
         <div>
-          <h4>{"Palette of colors:" + palette}</h4>
-          <ProgressiveImage placeholder="" src={fullImage}>
-              {(src, loading) => {
-                return loading ? compressedImage : <img src={src} alt="an image" />;
-              }}
-          </ProgressiveImage>
+          <img src={url} alt="test"/>
         </div>
     );
   }
