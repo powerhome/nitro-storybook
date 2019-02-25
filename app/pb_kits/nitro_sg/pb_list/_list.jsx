bf290efbe7e63fab9db9f892ba59ba45c75710e0
Jasper Furniss <jasper.furniss@powerhrg.com>
2019-02-25 15:50:39 -0500

    Latest ESLint fixes so that the List Component won't break

--- app/pb_kits/nitro_sg/pb_list/_list.jsx
+++ app/pb_kits/nitro_sg/pb_list/_list.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component } from "react"
 
 
 type Props = {
@@ -6,21 +6,22 @@ type Props = {
 }
 
 class List extends Component<Props> {
+
   static defaultProps = {
     //<!-- List prop default values here -->
   }
   props: Props
-  render() {  
+
+  render() {
 
     const {
       /*
-       Your props go here
+      Destructure props here
       */
     } = this.props
 
     return (
-      null
-      //<!-- Build component here -->
+      //<!-- Return component here -->
     )
   }
 }
